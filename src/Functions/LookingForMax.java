a3796742d9756ca10d917a09ef81aaa7c0ec1205
anatoly burtsev <anatolyburtsev@yandex.ru>
2016-03-05 00:24:47 +0300

    add recursion to fix solution for tricky function

--- src/Functions/LookingForMax.java
+++ src/Functions/LookingForMax.java
@@ -3,12 +3,20 @@ package Functions;
 public class LookingForMax {
 
     static double f(double x) {
-        return 10. - x*x;
+        if ( x < 0.8 && x> 0.75) {
+            return 10. - (x-1)*(x-1);
+        } else {
+            return 0;
+        }
+
     }
 
     static double lookingForMax(double x1, double x2, final double epsilon) {
         while (Math.abs(x2 - x1) > epsilon) {
             double medianValue = f(( x1 + x2 )/2);
+            if ( f(x1) == medianValue && f(x2) == medianValue ) {
+                return Math.max(lookingForMax(x1, (x1+x2)/2, epsilon), lookingForMax((x1+x2)/2, x2, epsilon));
+            }
             if ( f(x1) < medianValue ) {
                 x1 = ( x1 + x2 ) / 2;
             } else {
@@ -21,7 +29,7 @@ public class LookingForMax {
 
 
     public static void main(String[] args) {
-        double a = LookingForMax.lookingForMax(-10., -1., 0.01);
+        double a = LookingForMax.lookingForMax(-1., 1., 0.01);
         System.out.println(a);
     }
 
